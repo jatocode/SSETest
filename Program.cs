cef294cbf440f644078486596e359b69d4b9bff5
Tobias <tobias.jansson@gmail.com>
2025-12-21 17:51:08 +0100

    flyttade runt lite i backend

--- Program.cs
+++ Program.cs
@@ -5,17 +5,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(
-        policy =>
-        {
-            policy.WithOrigins("http://localhost:5173")
+builder.Services.AddCors(options => options.AddDefaultPolicy(
+        policy => policy.WithOrigins("http://localhost:5173")
                   .AllowAnyHeader()
                   .AllowAnyMethod()
-                  .AllowCredentials();
-        });
-});
+                  .AllowCredentials()
+));
 
 var app = builder.Build();
 
@@ -27,23 +22,26 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapGet("/", () => "SSE Test!");
+
 // Den här kommer direkt från Microsofts exempel på SSE med .NET 10
+// flyttade ut metoden bara 
 app.MapGet("/json-item", (CancellationToken cancellationToken) =>
 {
-    async IAsyncEnumerable<HeartRateRecord> GetHeartRate(
-        [EnumeratorCancellation] CancellationToken cancellationToken)
-    {
-        while (!cancellationToken.IsCancellationRequested)
-        {
-            var heartRate = Random.Shared.Next(60, 100);
-            yield return HeartRateRecord.Create(heartRate);
-            await Task.Delay(2000, cancellationToken);
-        }
-    }
-
     return TypedResults.ServerSentEvents(GetHeartRate(cancellationToken),
                                                   eventType: "heartRate");
 });
 
 app.UseCors();
 app.Run();
+
+async IAsyncEnumerable<HeartRateRecord> GetHeartRate(
+    [EnumeratorCancellation] CancellationToken cancellationToken)
+{
+    while (!cancellationToken.IsCancellationRequested)
+    {
+        var heartRate = Random.Shared.Next(60, 100);
+        yield return HeartRateRecord.Create(heartRate);
+        await Task.Delay(2000, cancellationToken);
+    }
+}
